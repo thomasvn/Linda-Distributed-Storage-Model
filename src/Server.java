710b02546304791cd304cecf663269ad12fe01d0
thomasvn <thomasnguyen96@gmail.com>
2017-04-07 12:31:07 -0700

    Initial commit

---
+++ src/Server.java
@@ -0,0 +1,13 @@
+public class Server {
+    // Create a Socket()
+
+    // Bind() the socket
+
+    // Tell server to Listen
+
+    // While Loop
+        // Accept()
+        // "Linda>" should be available
+            // Accepts commands "in, rd, out, add"
+        // Server should also always be listening for commands sent to it
+}
